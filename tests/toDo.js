28643f134d8a10c966aab51fe68a6cd5a57d0e00
Niranjana Nandu <niranjana.n@k2fly.com>
2025-06-24 11:58:37 +0200

    Encrypted the url and credentials, now safe to push to git

--- tests/toDo.js
+++ tests/toDo.js
@@ -3,11 +3,10 @@
    * Add more tests and helper methods
    * Setup different projects and environments/profiles
    * Make the data re-usable by making them dynamic - faker library
-   * Explore using fixtures for common setup 
-   * Figure out how to encrypt sensitive data like passwords -- Tried using dotnet-vault but can only configure it for one environment at a time on the free version. 
+   * Figure out how to encrypt sensitive data like passwords -- Tried using dotnet-vault but can only configure it for one environment at a time on the free version. --DONE
    * Will rather try using crypto-js and dotenv to encrypt and decrypt sensitive data.   *  -- DONE
    * Explore using Playwright's built-in test hooks for setup and teardown
-   * Explore using fixtures for common setup
+   * Explore using fixtures for common setup --DONE
    * Need to create a page object manager to manage all page objects -- DONE
    * Implement a logging mechanism for better debugging
    * Implement a retry mechanism for flaky tests
@@ -15,5 +14,5 @@
    * Implement a CI/CD pipeline to run tests automatically
    * Implement a test data management strategy to handle test data
    * Implement a test environment management strategy to handle different environments -- HANDLED BY USING ENVIRONMENT VARIABLES -- DONE
-   * Add a helper class that the pageObjects extend
+   * Add a helper class that the pageObjects extend --DONE
    * */
